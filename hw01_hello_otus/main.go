c54125b2726f71b8bd4380ea9e9ea294ab01e9ed
Peter Botov <p.botov@crpt.ru>
2022-02-24 21:39:49 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
+var sourceString = "Hello, OTUS!"
+
 func main() {
-	// Place your code here.
+	reversedStr := stringutil.Reverse(sourceString)
+	fmt.Println(reversedStr)
 }
