07e472a532609895632f56ff48e1fdc10560847d
Петр Ботов <p.botov@crpt.ru>
2022-08-09 19:16:46 +0300

    hw11 done

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,62 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/logger"
+)
+
+var timeout time.Duration
+
+const (
+	DefaultTimeout = 10
+	minArgsCnt     = 2
+)
+
+type Flags struct {
+	Timeout time.Duration
+}
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	Flags := new(Flags)
+	flag.DurationVar(&Flags.Timeout, "timeout", DefaultTimeout, "timeout for client in seconds")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) != minArgsCnt {
+		logger.Fatalf("not enough args passed. Expected %d, got %d", minArgsCnt, len(args))
+	}
+
+	c := NewTelnetClient(net.JoinHostPort(args[0], args[1]), Flags.Timeout, os.Stdin, os.Stdout)
+	if err := c.Connect(); err != nil {
+		logger.Fatalf("error connecting to host: %s", err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	go func() {
+		if err := c.Send(); err != nil {
+			logger.Errorf("error sending to host: %s", err)
+		} else {
+			logger.Infof("EOF")
+		}
+		cancel()
+	}()
+
+	go func() {
+		if err := c.Receive(); err != nil {
+			logger.Errorf("error receiving from host: %s", err)
+		} else {
+			logger.Infof("connection closed")
+		}
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,56 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *client) Connect() error {
+	if c.in == nil {
+		return fmt.Errorf("in is invalid")
+	}
+	if c.out == nil {
+		return fmt.Errorf("out is invalid")
+	}
+
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return err
+	}
+	if conn == nil {
+		return fmt.Errorf("connection not established")
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *client) Close() error {
+	return c.conn.Close()
+}
+
+func (c *client) Send() error {
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *client) Receive() error {
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return err
+	}
+	return nil
+}
